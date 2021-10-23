8a88607045a936e6eba56f209dd3170b6f71dd5b
James Brian <jamesbrian0323@gmail.com>
2021-10-23 06:05:38 -0400

    Add UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
+            UserSeeder::class,
             HomeLayoutSeeder::class,
             AboutusLayoutSeeder::class,
             WorkLayoutSeeder::class,
